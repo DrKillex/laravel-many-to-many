96d73275c4bc29e4736bc11f69423564d659bbd7
DrKillex <lcollaro1994@gmail.com>
2023-05-24 09:45:17 +0200

    small refactor

--- resources/views/admin/records/show.blade.php
+++ resources/views/admin/records/show.blade.php
@@ -7,20 +7,15 @@
                 <h2>title: {{ $record->title }}</h2>
             </li>
             <li>
-                <h3>type: <a
-                        href="@if ($record->type) {{ route('admin.types.show', $record->type) }} @endif">{{ $record->type?->name ?: 'Nessuna tipologia' }}</a>
-                </h3>
+                <h3>type: <a href="@if ($record->type) {{ route('admin.types.show', $record->type) }} @endif">{{ $record->type?->name ?: 'Nessuna tipologia' }}</a></h3>
             </li>
             <li>
                 <h3>technology:
-                    @if (count($record->technologies)>0)
-                        @foreach ($record->technologies as $tech)
-                            <a href="@if ($record->technologies) {{ route('admin.technologies.show', $tech) }} @endif">{{ $tech->name}}</a>
-                        @endforeach
-                    @else
-                    nessuna tecnologia
-                    @endif
-                
+                    @forelse ($record->technologies as $tech)
+                        <a href="{{ route('admin.technologies.show', $tech) }}">{{ $tech->name}}</a>
+                    @empty
+                    <span>nessuna tecnologia</span>
+                    @endforelse               
                 </h3>
             </li>
             <li>creation date: {{ $record->creation_date }}</li>
